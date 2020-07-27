6dab8ac9c8e37f9400a877655d65e4347a7c1c4f
Kevin Davis <kevincarrolldavis@gmail.com>
2020-07-27 08:33:39 -0400

    Typo

--- examples/battery_notify.py
+++ examples/battery_notify.py
@@ -4,17 +4,18 @@ from bleak import BleakClient
 address = "31EF2C41-7005-4195-846C-36CDB3B25619"
 BATTERY_LEVEL_UUID = "00002a19-0000-1000-8000-00805f9b34fb"
 
+
 async def run(address, loop):
     change = 0
 
     def battery_change(sender, value):
         battery_level = int.from_bytes(value, byteorder="little")
         print("Sender: {} set new value {}".format(sender, battery_level))
-        change = 1
+        change = 1  # noqa
 
     async with BleakClient(address, loop=loop) as client:
         await client.start_notify(BATTERY_LEVEL_UUID, battery_change)
-        print("Awating batter level change")
+        print("Awating battery level change")
 
         while change == 1:
             await asyncio.sleep(0.05)
